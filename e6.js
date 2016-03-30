a5b8cb1eb32c951cb8d2eb296e73c9a336e6703d
Leonardo Merza <ljmerza@gmail.com>
2016-03-30 01:52:27 +0000

    update1

--- e6.js
+++ e6.js
@@ -1,17 +1,11 @@
 var mongo = require('mongodb').MongoClient
 
-var url = 'mongodb://localhost:27017/learnyoumongo'
-
-var doc = {
-  firstName: process.argv[2]
-, lastName: process.argv[3]
-}
+var url = 'mongodb://localhost:27017/' + process.argv[2]
 
 mongo.connect(url, function(err, db) {
   if(err) throw err
-  db.collection('docs').insert(doc,function(err, data){
+  db.collection('users').update({name:'Tina'}, {$set:{age:40}},function(err, data){
       if(err) throw err
-      console.log(JSON.stringify(doc))
       db.close()
   })
 })
